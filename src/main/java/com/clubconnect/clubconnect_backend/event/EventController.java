3fff2fdb9834a378d2e16bdcf89699ca30b226b1
bhavindewani99 <bhavindewani@gmail.com>
2024-11-07 16:06:40 -0800

    Updated put api for events

--- src/main/java/com/clubconnect/clubconnect_backend/event/EventController.java
+++ src/main/java/com/clubconnect/clubconnect_backend/event/EventController.java
@@ -11,6 +11,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -54,7 +55,7 @@ public class EventController {
         return ResponseEntity.ok(eventDTO);
     }
 
-    // @PutMapping("/{id}")
+    @PutMapping("/{id}")
     public ResponseEntity<EventDTO> updateEvent(@PathVariable Long id, @RequestBody EventDTO eventDTO) {
         // Directly passing eventDTO to the service layer
         Event updatedEvent = eventService.updateEvent(id, eventDTO);
